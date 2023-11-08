2457d76d541c6515faad0d05834224c57a133823
Nicolás <nicolas.pavez@alumnos.ucn.cl>
2023-11-08 10:43:51 -0300

    Ejercicio 2

--- app/src/main/java/com/example/practica2dsm/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/practica2dsm/ui/dashboard/DashboardFragment.java
@@ -1,10 +1,14 @@
 package com.example.practica2dsm.ui.dashboard;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,18 +19,62 @@ import com.example.practica2dsm.databinding.FragmentDashboardBinding;
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
+    private EditText txt_rut, txt_name2, txt_age;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        txt_rut = binding.txtRut;
+        txt_age = binding.txtAge;
+        txt_name2 = binding.txtNombre2;
+
+        binding.btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveOnSharedPreference();
+                Toast.makeText(getContext(), "Datos guardados.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        binding.btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchUser();
+                Toast.makeText(getContext(), "Datos cargados.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        binding.btnPrevios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                previosUser();
+                Toast.makeText(getContext(), "Datos cargados.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return root;
+    }
+
+    private void previosUser() {
+    }
+
+    private void searchUser() {
+        SharedPreferences preferences = getActivity().getSharedPreferences("user", Context.MODE_PRIVATE);
+        String nombre = preferences.getString(txt_rut.getText().toString(), "");
+        String edad = preferences.getString(txt_rut.getText().toString()+"a", "");
+        txt_name2.setText(nombre);
+        txt_age.setText(edad);
+    }
+
+    private void saveOnSharedPreference() {
+        SharedPreferences pref = getActivity().getSharedPreferences("user", Context.MODE_PRIVATE);
+        SharedPreferences.Editor edt = pref.edit();
+        edt.putString(txt_rut.getText().toString(), txt_name2.getText().toString());
+        edt.putString(txt_rut.getText().toString()+"a", txt_age.getText().toString());
+        edt.commit();
     }
 
     @Override
